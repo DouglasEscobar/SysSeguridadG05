a4c1a7fbee304bbeaddc8a6f9fbae6a489501402
Douglas Escobar <DouglasEscobar27@outlook.com>
2024-02-19 15:54:42 -0600

    Se termino programacion de clase rol

---
+++ SysSeguridadG05.DAL/RolDAL.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//referencias
+using Microsoft.EntityFrameworkCore;
+using SysSeguridadG05.EN;
+
+namespace SysSeguridadG05.DAL
+{
+    public class RolDAL
+    {
+        public static async Task<int> CrearAsync(Rol pRol)
+        {
+            int result = 0;
+            using (var dbContext = new DBContexto())
+            {
+                dbContext.Add(pRol);
+                result = await dbContext.SaveChangesAsync();
+            }
+            return result;
+        }
+
+
+        public static async Task<int> ModificarAsync(Rol pRol)
+        {
+            int result = 0;
+            using (var dbContexto = new DBContexto())
+            {
+                var rol = await dbContexto.Rol.FirstOrDefaultAsync(s => s.Id == pRol.Id);
+                rol.Nombre = pRol.Nombre;
+                dbContexto.Update(rol);
+                result = await dbContexto.SaveChangesAsync();
+            }
+
+            return result;
+        }
+
+        
+        public static async Task<int> DeleteAsync(Rol pRol)
+        {
+            int result = 0;
+            using (var dbContexto = new DBContexto())
+            {
+                var rol = await dbContexto.Rol.FirstOrDefaultAsync(s => s.Id == pRol.Id);
+                //Eliminado logico
+                //rol.Estado = 2;
+                //dbContexto.Update(rol);
+                //result = await dbContexto.SaveChangesAsync();
+                //Fin eliminado logico
+
+                dbContexto.Rol.Remove(rol);
+                result = await dbContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<Rol> ObtenerPorIdAsync(Rol pRol)
+        {
+            Rol rol = new Rol();
+            using (var dbContexto = new DBContexto())
+            {
+                rol = await dbContexto.Rol.FirstOrDefaultAsync(s => s.Id == pRol.Id);
+            }
+            return rol;
+        }
+
+        public static async Task<List<Rol>> ObtenerTodosAsync()
+        {
+            List<Rol> roles = new List<Rol>();
+            using (var dbContexto = new DBContexto())
+            {
+                roles = await dbContexto.Rol.ToListAsync();
+            }
+            return roles;
+        }
+
+        internal static IQueryable<Rol> QuerySelect(IQueryable<Rol> pQuery, Rol pRol)
+        {
+            if (pRol.Id > 0)
+                pQuery = pQuery.Where(s => s.Id == pRol.Id);
+
+            if (!string.IsNullOrWhiteSpace(pRol.Nombre))
+                pQuery = pQuery.Where(s => s.Nombre.Contains(pRol.Nombre));
+            pQuery = pQuery.OrderBy(s => s.Id).AsQueryable();
+
+            if (pRol.Top_Aux > 0)
+                pQuery = pQuery.Take(pRol.Top_Aux).AsQueryable();
+            return pQuery;
+        }
+
+        public static async Task<List<Rol>> BuscarAsync(Rol pRol)
+        {
+            var roles = new List<Rol>();
+            using (var dbContexto = new DBContexto())
+            {
+                var select = dbContexto.Rol.AsQueryable();
+                select = QuerySelect(select, pRol);
+                roles = await select.ToListAsync();
+            }
+            return roles;
+        }
+    }
+}
